dfaccb5133c01fd134600dc04fb1c8991ee5ba80
MariaForester <maschalessnichaya@gmail.com>
2018-12-03 19:35:46 +0300

    more fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,5 @@ public class Main {
     public static void main(String[] args) {
         Drawer drawer = new Drawer();
         drawer.showTree(new SuffixTree("forester"));
-        drawer.showTree(new SuffixTree("aaaaaaaaaaaa"));
     }
 }
--- src/test/java/TreeTest.java
+++ src/test/java/TreeTest.java
@@ -40,5 +40,4 @@ public class TreeTest {
         assertFalse(tree.search("Fores", children));
         assertFalse(tree.search("j", children));
     }
-
 }
